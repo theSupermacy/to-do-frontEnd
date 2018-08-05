c470d1301c124c9337c303c596bc107da662bb2b
Rahul Sinha <rahulsinha@Rahuls-MacBook-Pro.local>
2018-08-06 00:39:09 +0530

    added automated story deployment

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
 import { configure } from '@storybook/react';
+import 'storybook-chromatic/storybook-addon';
 import '../src/index.css';
 const req = require.context('../src', true, /.stories.js$/);
 
